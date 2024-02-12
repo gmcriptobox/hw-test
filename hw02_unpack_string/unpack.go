bc87001f23e97c1a274840561a18f36b4f8f06db
ilya voynov <ilya.voynov@rtksoftlabs.ru>
2024-02-12 18:26:46 +0700

    fix

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,12 +9,27 @@ var ErrInvalidString = errors.New("invalid string")
 
 var isNumber = func(c rune) bool { return c >= '0' && c <= '9' }
 
-//var isSlash = func(c rune) bool { return c == '\\' }
-
 func appendToStrBuilder(strBuild *strings.Builder, char rune, count int) {
 	for i := 0; i < count; i++ {
 		strBuild.WriteRune(char)
 	}
+}
+
+func unpackOneCicle(strBuild *strings.Builder, runeSlice []rune, i int, c rune) bool {
+	if isNumber(c) {
+		if i > 0 {
+			if isNumber(runeSlice[i-1]) {
+				return false
+			}
+			appendToStrBuilder(strBuild, runeSlice[i-1], int(c-'0'))
+		} else {
+			//крайний случай, если первым символом идет цифра
+			return false
+		}
+	} else if i > 0 && !isNumber(runeSlice[i-1]) {
+		strBuild.WriteRune(runeSlice[i-1])
+	}
+	return true
 }
 
 func Unpack(str string) (string, error) {
@@ -24,21 +39,10 @@ func Unpack(str string) (string, error) {
 	}
 	builder := strings.Builder{}
 	runeSlice := []rune(str)
-	slashCount := 0
 	//идем по слайсу символов, при встрече числа добавляем n раз предыдущий символ
 	for i, c := range runeSlice {
-		if isNumber(c) {
-			if i > 0 {
-				if isNumber(runeSlice[i-1-slashCount]) {
-					return "", ErrInvalidString
-				}
-				appendToStrBuilder(&builder, runeSlice[i-1-slashCount], int(c-'0'))
-			} else {
-				//крайний случай, если первым символом идет цифра
-				return "", ErrInvalidString
-			}
-		} else if i > 0 && !isNumber(runeSlice[i-1]) {
-			builder.WriteRune(runeSlice[i-1])
+		if !unpackOneCicle(&builder, runeSlice, i, c) {
+			return "", ErrInvalidString
 		}
 	}
 	lenSlice := len(runeSlice)
