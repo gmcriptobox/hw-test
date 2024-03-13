573115d7d2fc5b3b06675fc15946b5b1ba172a4f
ilya voynov <ilya.voynov@rtksoftlabs.ru>
2024-03-13 13:33:41 +0700

    hw6 первичная обработка

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,9 @@
 package hw06pipelineexecution
 
+import (
+	"fmt"
+)
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -9,6 +13,44 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	outSlise := make([]Out, 0, len(stages))
+	outSliseFlag := make([]bool, 0, len(stages))
+	result := make(Bi, len(stages)+1)
+	defer close(result)
+	for task := range in {
+		out := make(Bi, 1)
+		out <- task
+		in = In(out)
+		for _, s := range stages {
+			in = s(in)
+		}
+		outSlise = append(outSlise, in)
+		outSliseFlag = append(outSliseFlag, false)
+	}
+	for {
+		select {
+		case _, ok := <-done:
+			if !ok {
+				return result
+			}
+		default:
+		}
+		for i := 0; i < len(outSlise); i++ {
+			select {
+			case x := <-outSlise[i]:
+				outSliseFlag[i] = true
+				result <- x
+				fmt.Println(x)
+			default:
+			}
+		}
+		quit := true
+		for _, f := range outSliseFlag {
+			quit = quit && f
+		}
+		if quit {
+			break
+		}
+	}
+	return result
 }
